917041db4efa59a4e71b5bc2061c93a21c4dcd2f
Ofovwe Ewere <ewereeman@gmail.com>
2021-06-04 20:03:49 -0500

    all sample pages created

--- .history/Server/Routes/index_20210603164402.ts
+++ .history/Server/Routes/index_20210603164402.ts
@@ -13,22 +13,22 @@ router.get('/home', function(req, res, next) {
 
 /* GET about page. */
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About Me', page: 'about' });
+  res.render('index', { title: 'About Us', page: 'about' });
 });
 
 /* GET projects page. */
 router.get('/projects', function(req, res, next) {
-  res.render('index', { title: 'My Projects', page: 'projects' });
+  res.render('index', { title: 'Our Projects', page: 'projects' });
 });
 
 /* GET services page. */
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'My Services', page: 'services' });
+  res.render('index', { title: 'Our Services', page: 'services' });
 });
 
 /* GET contact page. */
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact Me', page: 'contact' });
+  res.render('index', { title: 'Contact Us', page: 'contact' });
 });
 
 //module.exports = router;
